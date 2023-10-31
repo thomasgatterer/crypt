b4bd8b6b003cec5e6ce49039609d7ffb5fc300d9
thomasgatterer <t.gatterer@tsn.at>
2023-10-31 18:34:38 +0100

    caesarCryptLetter first attempt

---
+++ methos.py
@@ -0,0 +1,25 @@
+## https://www.w3schools.com/python/ref_string_maketrans.asp
+## https://www.w3schools.com/python/ref_string_translate.asp
+## https://www.askpython.com/python/examples/when-to-use-colon-python
+
+
+
+## https://pythontutor.com/render.html#mode=display
+
+
+import string
+
+plaintext = "ABCD"
+shift = "D"
+alphabet = string.ascii_lowercase+string.ascii_uppercase
+
+##shiftNumber = 3
+##print(alphabet[shiftNumber:])
+##print(alphabet[:shiftNumber])
+
+##print(alphabet.index("D"))
+
+
+shifted_alphabet = alphabet[alphabet.index(shift):] + alphabet[:alphabet.index(shift)]
+table = str.maketrans(alphabet, shifted_alphabet)
+print(plaintext.translate(table))
